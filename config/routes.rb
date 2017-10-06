b8a672dea1904cc840ecf4b49e9460edf0ad1b1b
Karim Elnadim <kelnadim@bbasolutions.com>
2017-10-06 07:59:30 -0500

    disable turbo link add mdl gem add dashboard toolbars template

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :want_list_items
   resources :want_lists
-  root 'users#index'
+  root 'dashboards#index'
   devise_for :users
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
