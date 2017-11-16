d0308c00f1105d51917c0fd4b3151c3e6666d7ce
Karim Elnadim <kelnadim@bbasolutions.com>
2017-11-16 13:21:29 -0600

    remove MarketOrderItemNumber allow null buyer email

--- db/migrate/20171027152326_create_indaba_orders.rb
+++ db/migrate/20171027152326_create_indaba_orders.rb
@@ -3,11 +3,10 @@ class CreateIndabaOrders < ActiveRecord::Migration[5.1]
     create_table :indaba_orders do |t|
       t.references :book, foreign_key: true, null: false, index: true
       t.references :indaba_instance, foreign_key: true, null: false, index: true
-      t.string :market_book_order_id, null: false, index: true
       t.float :price_paid, null: false, default: 0.00
       t.string :market_name, null: false, index: true
-      t.string :buyer_email, null: false, index: true
-      t.string :market_order_item_number, null: false, index: { unique: true }
+      t.string :buyer_email, null: true, index: true
+      t.string :market_book_order_id, null: false, index: true
       t.datetime :date_ordered, null: false, index: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -88,11 +88,10 @@ ActiveRecord::Schema.define(version: 20171031155344) do
   create_table "indaba_orders", force: :cascade do |t|
     t.bigint "book_id", null: false
     t.bigint "indaba_instance_id", null: false
-    t.string "market_book_order_id", null: false
     t.float "price_paid", default: 0.0, null: false
     t.string "market_name", null: false
-    t.string "buyer_email", null: false
-    t.string "market_order_item_number", null: false
+    t.string "buyer_email"
+    t.string "market_book_order_id", null: false
     t.datetime "date_ordered", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -103,7 +102,6 @@ ActiveRecord::Schema.define(version: 20171031155344) do
     t.index ["indaba_instance_id"], name: "index_indaba_orders_on_indaba_instance_id"
     t.index ["market_book_order_id"], name: "index_indaba_orders_on_market_book_order_id"
     t.index ["market_name"], name: "index_indaba_orders_on_market_name"
-    t.index ["market_order_item_number"], name: "index_indaba_orders_on_market_order_item_number", unique: true
   end
 
   create_table "users", force: :cascade do |t|
