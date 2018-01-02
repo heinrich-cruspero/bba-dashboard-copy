e796e51b550418879ec71dbebce13eec8d41a56e
Karim Elnadim <kelnadim@bbasolutions.com>
2018-01-02 16:58:38 -0600

    update shared want list logic to use shared privacy

--- app/assets/javascripts/want_list_form.js
+++ app/assets/javascripts/want_list_form.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
     $('input[name="want_list[want_list_privacy_id]"]').on('change', function(){
-        if ($(this).val() == 2) {
+        if ($(this).val() == 3) {
             $('#users_select_div').attr('hidden', false);
             $('input[name="want_list[user_ids][]"]').attr('disabled', false);
         }
-        else if ($(this).val() == 1) {
+        else {
             $('#users_select_div').attr('hidden', true);
             $('input[name="want_list[user_ids][]"]').attr('disabled', false);
             $("#users_select option:selected").removeAttr("selected");
