ff20536d10059e968c3852f4deca9535291dd00a
Karim Elnadim <kelnadim@bbasolutions.com>
2018-01-04 08:40:07 -0600

    fix public want-lists item viewing and editing

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,10 +10,12 @@ class Ability
       can [:create], WantListItem
       can [:update, :destroy], WantListItem, :user => user
       can [:update], WantListItem, want_list_id: user.want_lists.pluck(:id)
+      can [:update], WantListItem, want_list_id: WantList.where(want_list_privacy_id: 1).pluck(:id)
 
       can [:index, :create], WantList
       can [:update, :destroy, :items], WantList, :owner => user
       can [:items], WantList, id: user.want_lists.pluck(:id)
+      can [:items], WantList, want_list_privacy_id: 1
     end
   end
 end
