61b00d2ce9d546206ad229394377f68ef4b452db
Karim Elnadim <kelnadim@bbasolutions.com>
2018-04-11 12:02:24 -0500

    add want list export

--- app/models/want_list_item.rb
+++ app/models/want_list_item.rb
@@ -6,4 +6,26 @@ class WantListItem < ApplicationRecord
   has_one :user, :through => :want_list, :source => 'owner'
 
   belongs_to :book, :foreign_key => :ean, :primary_key => :ean, optional: true
+
+  def self.to_csv
+    attributes = %w{ean quantity quantity_purchased max_price book.author book.title}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.each do |want_list_item|
+        csv << attributes.map do |attr|
+          if attr.split('.').count > 1
+            if want_list_item.send(attr.split('.')[0]).nil?
+              ''
+            else
+              want_list_item.send(attr.split('.')[0]).send(attr.split('.')[1])
+            end
+          else
+            want_list_item.send(attr)
+          end
+        end
+      end
+    end
+  end
 end
