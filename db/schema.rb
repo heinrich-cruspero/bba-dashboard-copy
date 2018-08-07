bf4666868e1b054b74c5985926b6da93f04afbac
Karim Elnadim <kelnadim@bbasolutions.com>
2018-08-07 11:16:13 -0500

    add vendor_ids migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180703143703) do
+ActiveRecord::Schema.define(version: 20180807161410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180703143703) do
     t.string "cc_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "vendor_ids"
   end
 
   create_table "accounts", force: :cascade do |t|
