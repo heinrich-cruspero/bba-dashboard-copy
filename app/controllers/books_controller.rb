8d4fac7fc0771254e8b498b6201cdf1ea6019aed
Karim Elnadim <karim@rev365.com>
2020-05-11 07:12:37 -0500

    Import missing books from DataWh when searched

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,27 +5,13 @@ class BooksController < ApplicationController
   load_and_authorize_resource
 
   before_action :set_book, only: [:details]
+  before_action :import_book, only: [:index]
 
   # GET /books
   def index
     respond_to do |format|
       format.html
-      format.json {
-        search_length = params[:search][:value].length
-        if [10,13].include? params[:search][:value].length
-          case search_length
-          when 10
-            result = Book.find_by_isbn(params[:search][:value])
-          when 13
-            result = Book.find_by_ean(params[:search][:value])
-          end
-
-          if result.nil?
-            Book.create(ean: '1111111111111', isbn: '1111111111', author: '', title: '')
-          end
-        end
-        render json: BookDatatable.new(view_context)
-      }
+      format.json { render json: BookDatatable.new(view_context) }
     end
   end
 
@@ -44,5 +30,35 @@ class BooksController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def book_params
     params.require(:book).permit
+  end
+
+  def import_book
+    return if params[:search].nil?
+
+    val = params[:search][:value]
+
+    val_length = val.length
+
+    case val_length
+    when 10
+      book = Book.find_by_isbn(val)
+      field = 'isbn'
+    when 13
+      book = Book.find_by_ean(val)
+      field = 'ean'
+    else
+      return
+    end
+
+    return unless book.nil?
+
+    result = DataWhApiService.new.get_book(field, val)
+
+    return if result.empty?
+
+    Book.create(ean: result[0]['ean'], isbn: result[0]['isbn'],
+                author: result[0]['author'], title: result[0]['title'],
+                publisher:  result[0]['publisher'], edition: result[0]['edition'],
+                publication_date: result[0]['publication_date'])
   end
 end
