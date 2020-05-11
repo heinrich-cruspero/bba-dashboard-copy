a70ab2d4667e6da819288b58d82da59dc6eed722
Karim Elnadim <karim@rev365.com>
2020-05-11 08:02:52 -0500

    updated query to ensure parentheses

--- app/models/valore_order.rb
+++ app/models/valore_order.rb
@@ -2,14 +2,13 @@
 
 ##
 class ValoreOrder < ApplicationRecord
-
-    scope :valore_pending_count, -> {
-      select(:isbn).group(:isbn)
-      .where(status: ['pending', 'in-transit'])
-      .where("created_at >= ?", (Time.now - 15.days))
-      .or(ValoreOrder.select(:isbn).group(:isbn).where(status: ['accepted'])
-      .where("created_at >= ?", (Time.now - 24.hours)))
-      .count
-    }
-
+  scope :valore_pending_count, lambda {
+    select(:isbn).group(:isbn)
+                 .where('(status IN (?) AND created_at >= ?) OR (status = ? AND created_at >= ?)',
+                        ['pending', 'in-transit'],
+                        (Time.now - 15.days),
+                        'accepted',
+                        (Time.now - 24.hours))
+                 .count
+  }
 end
