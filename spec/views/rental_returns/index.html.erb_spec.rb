49027b8ed10df38a857d3f8d1783d3a477808851
Karim Elnadim <karim@rev365.com>
2020-06-08 09:13:45 -0500

    fix specs

--- spec/views/rental_returns/index.html.erb_spec.rb
+++ spec/views/rental_returns/index.html.erb_spec.rb
@@ -4,7 +4,11 @@ require 'rails_helper'
 
 RSpec.describe 'rental_returns/index', type: :view do
   it 'renders rental_returns index' do
+    current_user = create(:user)
+    allow(view).to receive(:current_user).and_return(current_user)
+
     render
+
     expect(rendered).to have_selector('#rental_returns-table')
   end
 end
