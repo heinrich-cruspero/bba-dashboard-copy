bf85bdc7d96345068b213dbb193194652ec90d03
Ramya <ramya@bbasolutions.com>
2020-08-03 23:00:15 -0500

    add alert for account checkbox

--- app/assets/javascripts/reporting_pages_date_range.js
+++ app/assets/javascripts/reporting_pages_date_range.js
@@ -1,20 +1,26 @@
-jQuery(document).ready(function() {
-    $(function() {
-        $( "#from_date" ).datepicker({
-            dateFormat: 'mm-dd-yy',
-            changeMonth: true,
-            numberOfMonths: 2,
-            onClose: function( selectedDate ) {
-                $( "#to_date" ).datepicker( 'option', 'minDate', selectedDate );
-                $( "#to_date" ).datepicker( 'option', 'disabled', false );
-                $( "#to_date").focus();
-            }
-        });
-        $( "#to_date" ).datepicker({
-            dateFormat: 'mm-dd-yy',
-            changeMonth: true,
-            numberOfMonths: 2,
-            disabled: true,
-        });
+$(function() {
+    $( "#from_date" ).datepicker({
+        dateFormat: 'mm-dd-yy',
+        changeMonth: true,
+        numberOfMonths: 2,
+        onClose: function( selectedDate ) {
+            $( "#to_date" ).datepicker( 'option', 'minDate', selectedDate );
+            $( "#to_date" ).datepicker( 'option', 'disabled', false );
+            $( "#to_date").focus();
+        }
+    });
+    $( "#to_date" ).datepicker({
+        dateFormat: 'mm-dd-yy',
+        changeMonth: true,
+        numberOfMonths: 2,
+        disabled: true,
+    });
+
+    $('#search_valore_orders').click(function() { 
+        checked = $("input[type=checkbox]:checked").length;  
+        if(!checked) { 
+            alert("Please select at least one account"); 
+            return false; 
+        }  
     });
 });
