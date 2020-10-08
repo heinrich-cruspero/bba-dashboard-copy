3a3441bec9dec1ac2f68fadcd316671307daaa2e
Karim Elnadim <karim@rev365.com>
2020-10-08 15:55:17 -0500

    update subscription call backs

--- app/assets/javascripts/channels/file_download_channel.js
+++ app/assets/javascripts/channels/file_download_channel.js
@@ -1,17 +1,17 @@
 $(function() {
     $('#export-valore-orders-button').on('click', function(e) {
         App.cable.subscriptions.create("FileDownloadChannel", {
-            connected() {
+            connected: function() {
                 // Called when the subscription is ready for use on the server
                 console.log('Connected to FileDownloadChannel')
             },
 
-            disconnected() {
+            disconnected: function() {
                 // Called when the subscription has been terminated by the server
                 console.log('Disconnected from FileDownloadChannel')
             },
 
-            received(data) {
+            received: function(data) {
                 // Called when there's incoming data on the websocket for this channel
                 console.log('Received on FileDownloadChannel')
 
