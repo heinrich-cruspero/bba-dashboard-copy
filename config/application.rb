bb28b5a0701c165d4f124d2cab74d4a4053e955e
Karim Elnadim <karim@rev365.com>
2021-05-18 11:28:25 -0500

    update smtp address

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module BbaDashboard
     # -- all .rb files in that directory are automatically loaded.
 
     config.action_mailer.smtp_settings = {
-      address: 'email-smtp.us-east-1.amazonaws.com',
+      address: 'email-smtp.us-west-2.amazonaws.com',
       port: 587,
       user_name: ENV['SES_SMTP_USERNAME'], # Your SMTP user
       password: ENV['SES_SMTP_PASSWORD'], # Your SMTP password
